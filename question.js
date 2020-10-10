c848c5cd8f731ba410999529ea01fc821e72d8ab
Jean <jeansud@hotmail.com>
2020-10-10 19:29:49 -0300

    Redondinhoooo!

--- question.js
+++ question.js
@@ -1,29 +1,29 @@
-var question = [{
-    "question": "Você teve febre?",
-    "option1": "Sim",
-    "option2": "Não",
-    "option3": "Apenas 1 dia",
-    "option4": "A semana toda",
-    "answer": "2"
+var questions = [{
+    "question": "HTML é uma linguagem de...",
+    "option1": "Criação",
+    "option2": "Programação",
+    "option3": "Exclusão",
+    "option4": "Formatação",
+    "answer": "4"
 }, {
-    "question": "Você teve dor de garganta?",
-    "option1": "Sim",
-    "option2": "Não",
-    "option3": "Apenas 1 dia",
-    "option4": "A semana toda",
+    "question": "A tag para adição do estilo CSS fica entre que tags?",
+    "option1": "<body></body>",
+    "option2": "<title></title>",
+    "option3": "<head></head>",
+    "option4": "<html></title> ",
     "answer": "3"
 }, {
-    "question": "Você teve falta de ar?",
-    "option1": "Sim",
-    "option2": "Não",
-    "option3": "Apenas 1 dia",
-    "option4": "A semana toda",
+    "question": "Quando o programador deseja aplicar uma formatação na tag, esse processo é chamado de estilo...",
+    "option1": "In-line",
+    "option2": "Incorporado",
+    "option3": "Exclusão",
+    "option4": "Formatação",
     "answer": "1"
 }, {
-    "question": "Você teve falta de ar?",
-    "option1": "Sim",
-    "option2": "Não",
-    "option3": "Apenas 1 dia",
-    "option4": "A semana toda",
-    "answer": "4"
+    "question": "Qual é a função da tag DD?",
+    "option1": "Lista de definição?",
+    "option2": "Elemento termo",
+    "option3": "Termo",
+    "option4": "Definição Elemento",
+    "answer": "2"
 }];
