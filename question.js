19b425822f9407039f24e026931ff928be08aa10
Jean <jeansud@hotmail.com>
2020-10-10 10:06:58 -0300

    aula

---
+++ question.js
@@ -0,0 +1,29 @@
+var question = [{
+    "question": "Você teve febre?",
+    "option1": "Sim",
+    "option2": "Não",
+    "option3": "Apenas 1 dia",
+    "option4": "A semana toda",
+    "answer": "2"
+}, {
+    "question": "Você teve dor de garganta?",
+    "option1": "Sim",
+    "option2": "Não",
+    "option3": "Apenas 1 dia",
+    "option4": "A semana toda",
+    "answer": "3"
+}, {
+    "question": "Você teve falta de ar?",
+    "option1": "Sim",
+    "option2": "Não",
+    "option3": "Apenas 1 dia",
+    "option4": "A semana toda",
+    "answer": "1"
+}, {
+    "question": "Você teve falta de ar?",
+    "option1": "Sim",
+    "option2": "Não",
+    "option3": "Apenas 1 dia",
+    "option4": "A semana toda",
+    "answer": "4"
+}];
